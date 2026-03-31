1de3cbb313af065da9bde084416b3c3177075ec3
MarkoKrznar <mk1185@rit.edu>
2026-03-31 17:51:02 +0200

    trying fix

--- src/Phase1/Model/RecipeFactory.java
+++ src/Phase1/Model/RecipeFactory.java
@@ -15,21 +15,16 @@ public class RecipeFactory implements FoodFactory {
 
         // make a new recipe
         Recipe recipe = new Recipe(name);
-        /**
-         * if theres ingredients we add them
-         * format should be: "ingredientName:servings"
-         */
-        for (int i = 2; i < parts.length; i++) { // starting index 2 
-            String[] ingParts = parts[i].split(":"); // split ingredientnae:serving into 2 parts
-            if (ingParts.length == 2) {
-                String ingredientName = ingParts[0]; // get ingredient name
-                double servings = Double.parseDouble(ingParts[1]); // get serving
-                FoodComponent ingredient = foodCollection.getFood(ingredientName);
-                if (ingredient != null) {
-                    recipe.addIngredient(ingredient, servings);
-                }
-                //ingredient not found, just skip it 
+
+        // ingredients are alternating name,count pairs: name1, count1, name2, count2, ...
+        for (int i = 2; i + 1 < parts.length; i += 2) { // i is 2 cause index 0 is type and index 1 is name
+            String ingredientName = parts[i]; // ingredient 
+            double servings = Double.parseDouble(parts[i + 1]); // count of sreving
+            FoodComponent ingredient = foodCollection.getFood(ingredientName);
+            if (ingredient != null) {
+                recipe.addIngredient(ingredient, servings);
             }
+            // ingredient not found, just skip it
         }
 
         return recipe;
